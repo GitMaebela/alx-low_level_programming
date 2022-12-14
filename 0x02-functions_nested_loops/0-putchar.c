40f26ed7bd924e19bfe321f2e2e66769a96a62ac
Milwaukee Maebela <maebelam@gmail.com>
2022-12-14 12:10:30 -0800

    feat:

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - _putchar, followed by a new line.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	putchar('_putchar\n');
+	putchar("\n");
+	return (0);
+}
