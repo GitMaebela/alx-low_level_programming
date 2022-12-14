38a05214dd0f256bba812a9d4074cf96b8c77088
Milwaukee Maebela <maebelam@gmail.com>
2022-12-14 13:31:32 -0800

    feat: add a function that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _abs - computes the absolute value of an integer
+*
+* @a: num
+*
+* Return: result
+*/
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (a * -1);
+	}
+}
